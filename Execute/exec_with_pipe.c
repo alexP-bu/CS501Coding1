69b668a46a83579c54fb2d9e3a772d09f9482dd0
alexP-bu <apozin@bu.edu>
2022-11-06 18:27:49 -0500

    working on exec with pipe

--- Execute/exec_with_pipe.c
+++ Execute/exec_with_pipe.c
@@ -9,13 +9,18 @@ int main(int argc, char* argv[]){
         return 0;
     }
 
-
     // parse args
     char* program = argv[1];
     char* args = argv[2];
 
     // create buffer for cmdline argument
-    // //your solution here!
+    char* cmd = (char *)malloc(
+        strlen("cmd.exe /c") + 
+        strlen(program) + 
+        strlen(args) + 
+        1 //space between %s
+        );
+    sprintf(cmd, "cmd.exe /c %s %s", program, args);
     
     // Declare handles for StdOut
     HANDLE hStdOutRead, hStdOutWrite;
@@ -25,32 +30,32 @@ int main(int argc, char* argv[]){
     ZeroMemory(&pi, sizeof(pi));
     ZeroMemory(&si, sizeof(si));
     si.cb = sizeof(si);
-    // TODO: Set si.dwFlags...
+    // Set si.dwFlags...
     // HINT Read this and look for anything that talks about handle inheritance :-)
     //  https://docs.microsoft.com/en-us/windows/win32/api/processthreadsapi/ns-processthreadsapi-startupinfoa
-    
-    // //your solution here!
-
+    si.dwFlags = STARTF_USESTDHANDLES;
 
     SECURITY_ATTRIBUTES sa;
     sa.nLength = sizeof(sa);
     sa.lpSecurityDescriptor = NULL;
 
-    // TODO: ensure that the child processes can inherit our handles!
-    // //your solution here!
+    // nsure that the child processes can inherit our handles!
+    sa.bInheritHandle = TRUE;
+    // Create a pipe  object and share the handle with a child processes 
+    DWORD nSize = BUF_SIZE;
+    if(!CreatePipe(hStdOutRead, hStdOutWrite, &sa, nSize)){
+        printf("[!] FAILED TO CREATE PIPE!");
+    }
 
-    // TODO: Create a pipe  object and share the handle with a child processes 
-    // //your solution here!
-
-    // TODO: Set
     // set startupinfo handles
     si.hStdInput = NULL;
     si.hStdError = hStdOutWrite;
     si.hStdOutput = hStdOutWrite;
-
     
     // Create the child Processes and wait for it to terminate!
-    // //your solution here!
+    if(!CreateProcessA(NULL, cmd, NULL, NULL, TRUE, 0, NULL, NULL, &si, &pi)){
+        printf("[!] FAILED TO START PROCESS!");
+    }
 
     // TODO: perform any cleanup necessary! 
     // The parent processes no longer needs a handle to the child processes, the running thread, or the out file!
